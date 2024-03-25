611b4549414a8c73630d092db25e33b9ec9b5935
Aryan <aryansck@gmail.com>
2024-03-25 17:03:50 +0330

    angular material and tailwindcss installed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const { join } = require('path');
+
+module.exports = {
+  content: [join(__dirname, 'src/**/*.{html,ts}')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
