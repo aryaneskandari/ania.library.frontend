c7bf305b2862d34ac0593c82fdad25a91165a891
Aryan <aryansck@gmail.com>
2024-03-25 17:22:06 +0330

    LibrariansHome,LibrariansBooks,LibrariansMembers,LibrariansBooks component are created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PublicNavComponent } from './+pages/+public/public-nav/public-nav.component';
+import { LibrariansNavComponent } from './+pages/+librarians/librarians-nav/librarians-nav.component';
+import { AdminsNavComponent } from './+pages/+admins/admins-nav/admins-nav.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'public',component:PublicNavComponent},
+  {path:'librarians',component:LibrariansNavComponent},
+  {path:'admins',component:AdminsNavComponent},
+  {path:'',redirectTo:'/public',pathMatch:'full'},
+  {path:'**',redirectTo:'/public'}
+];
